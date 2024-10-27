54e78c1d39c41f2d8a16dc55f5c806a5ba64d792
Emilio0813 <353964@via.dk>
2024-10-27 20:19:44 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 namespace SpriteKind {
     export const Enemy2 = SpriteKind.create()
+    export const Enemy3 = SpriteKind.create()
+    export const Enemy4 = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     Skud_retning = 3
@@ -96,11 +98,27 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             `, mySprite, 0, 100)
     }
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy3, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+    info.changeLifeBy(-1)
+    mySprite.setPosition(25, 25)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy3, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.hearts, 100)
+    info.changeScoreBy(1)
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     Skud_retning = 1
 })
 info.onScore(30, function () {
     tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairLarge)
+})
+info.onScore(100, function () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy3)
+    Enemy_Sprite_4 = sprites.create(assets.image`myImage2`, SpriteKind.Enemy4)
+    tiles.placeOnRandomTile(Enemy_Sprite_4, sprites.castle.rock1)
+    scaling.scaleByPercent(Enemy_Sprite_4, 300, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    mySprite.setPosition(0, 0)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     Skud_retning = 2
@@ -137,8 +155,10 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     info.changeLifeBy(-1)
     mySprite.setPosition(25, 25)
 })
+let Enemy_Sprite_3: Sprite = null
 let Enemy_sprite2: Sprite = null
 let Enemy_sprite: Sprite = null
+let Enemy_Sprite_4: Sprite = null
 let projectile: Sprite = null
 let Skud_retning = 0
 let Current_level = 0
@@ -161,5 +181,13 @@ game.onUpdateInterval(500, function () {
         Enemy_sprite2 = sprites.create(assets.image`myImage3`, SpriteKind.Enemy2)
         tiles.placeOnRandomTile(Enemy_sprite2, sprites.dungeon.collectibleBlueCrystal)
         Enemy_sprite2.follow(mySprite, 30)
+    } else if (Current_level == 3) {
+        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy2)
+        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+        Enemy_Sprite_3 = sprites.create(assets.image`myImage2`, SpriteKind.Enemy3)
+        tiles.placeOnRandomTile(Enemy_Sprite_3, sprites.dungeon.stairLadder)
+        Enemy_Sprite_3.follow(mySprite, 30)
+    } else {
+    	
     }
 })
